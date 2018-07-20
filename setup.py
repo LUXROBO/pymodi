956b7e8d2050829c426bf964ba7aa929d53facf8
koriel <koriel@luxrobo.com>
2018-07-20 13:32:32 +0900

    pyserial을 setup.py > requirements에 추가

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = [
+    'Click>=6.0',
+    'pyserial>=3.1.1'
+    ]
 
 setup_requirements = [ ]
 
