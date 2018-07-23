45321803092b284c71c7ba0206d28581b1c65d30
koriel <koriel@luxrobo.com>
2018-07-23 14:16:55 +0900

    Remove python 3.7 from supported versions

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     entry_points={
