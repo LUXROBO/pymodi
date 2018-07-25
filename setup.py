e888d81b608c7508bfcb048ad2614d25d7b02e84
koriel <koriel@luxrobo.com>
2018-07-25 16:57:24 +0900

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/LUXROBO/MODI-Python-API',
-    version='0.1.2',
+    version='0.2.0',
     zip_safe=False,
 )
