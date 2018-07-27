bb3e51454fe88f7a6600535541fb78aa9d838bb2
koriel <koriel@luxrobo.com>
2018-07-27 22:57:58 +0900

    Bump version: 0.3.0 → 0.3.1

--- modi/__init__.py
+++ modi/__init__.py
@@ -6,6 +6,6 @@ from __future__ import absolute_import
 
 __author__ = """Jinsoo Heo"""
 __email__ = 'koriel@luxrobo.com'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 __all__ = ['modi', 'serial']
