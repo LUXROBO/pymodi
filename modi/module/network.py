417e9c123bdcd07f89dd051c8613428fb8a22ba9
koriel <koriel@luxrobo.com>
2018-08-17 16:22:05 +0900

    network 모듈 추가

---
+++ modi/module/network.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Network module."""
+
+from __future__ import absolute_import
+
+from enum import Enum
+
+from modi.module._module import SetupModule
+
+class PropertyType(Enum):
+    RESERVED = 0
+
+class Network(SetupModule):
+    property_types = PropertyType
+
+    def __init__(self, id, uuid, modi):
+        super(Network, self).__init__(id, uuid, modi)
+        self._type = "network"
