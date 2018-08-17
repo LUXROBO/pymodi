ac4a660887a7659dc154a16dc50e506186d5cb24
koriel <koriel@luxrobo.com>
2018-08-17 16:06:59 +0900

    modi.module 모듈의 하위 패키지 class import

--- modi/module/__init__.py
+++ modi/module/__init__.py
@@ -4,6 +4,17 @@
 
 from __future__ import absolute_import
 
+from modi.module.button import Button
+from modi.module.dial import Dial
+from modi.module.display import Display
+from modi.module.env import Env
+from modi.module.gyro import Gyro
+from modi.module.ir import Ir
+from modi.module.mic import Mic
+from modi.module.motor import Motor
+from modi.module.speaker import Speaker
+from modi.module.ultrasonic import Ultrasonic
+
 __author__ = """Jinsoo Heo"""
 __email__ = 'koriel@luxrobo.com'
 __version__ = '0.3.1'
