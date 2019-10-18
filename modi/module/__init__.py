8e2fd9881647315936c5d29b8ea963cc5854384b
koriel <koriel@luxrobo.com>
2019-10-18 14:59:38 +0900

    Bump version: 0.5.2 → 0.5.3

--- modi/module/__init__.py
+++ modi/module/__init__.py
@@ -17,6 +17,6 @@ from modi.module.ultrasonic import Ultrasonic
 
 __author__ = """Jinsoo Heo"""
 __email__ = 'koriel@luxrobo.com'
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 __all__ = ["env", "gyro", "mic", "button", "dial", "ultrasonic", "ir", "display", "motor", "led", "speaker"]
