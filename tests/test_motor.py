aec55a7df43dccd2a11dade3b7ed73aa21811c0c
Jinsung Ha <jinsung.ha929@gmail.com>
2020-01-21 17:43:17 +0900

    Complete test case for motor class

--- tests/test_motor.py
+++ tests/test_motor.py
@@ -27,24 +27,35 @@ class TestMotor(unittest.TestCase):
 
     def test_init(self):
         """Test initialization of motor module"""
+        print("run test_init")
         self.assertIsInstance(self.motor, Motor)
 
     def test_torque(self):
         """Test torque method"""
+        print("run test_torque")
         pass
 
     def test_speed(self):
         """Test speed method"""
+        print("run test_speed")
         expected_speeds = (30, 30)
         self.motor.speed(*expected_speeds)
-        time.sleep(3)
+        time.sleep(2)
         actual_speeds = self.motor.speed()
         self.assertEqual(expected_speeds, actual_speeds)
 
     def test_degree(self):
         """Test degree method"""
+        print("run test_degree")
         expected_degrees = (50, 50)
-        self.motor.degree
+        self.motor.degree(*expected_degrees)
+        time.sleep(2)
+        actual_degrees = self.motor.degree()
+        self.assertEqual(expected_degrees, actual_degrees)
+
+    def test_individual_control(self):
+        """Test individual motor control method"""
+        pass
 
 
 if __name__ == "__main__":
