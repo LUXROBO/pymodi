233cf6fe2ecc68334cdb560a36825100a700f90b
Jinsung Ha <jinsung.ha929@gmail.com>
2020-02-04 18:01:51 +0900

    Implement testcase testing for get module list function

--- tests/test_modi.py
+++ tests/test_modi.py
@@ -4,6 +4,21 @@ from unittest import mock
 
 from modi.modi import MODI
 
+from modi.module.setup_module.setup_module import SetupModule
+from modi.module.input_module.input_module import InputModule
+from modi.module.output_module.output_module import OutputModule
+
+from modi.module.input_module.button import Button
+from modi.module.input_module.dial import Dial
+from modi.module.input_module.env import Env
+from modi.module.input_module.gyro import Gyro
+from modi.module.input_module.ir import Ir
+
+from modi.module.output_module.display import Display
+from modi.module.output_module.led import Led
+from modi.module.output_module.motor import Motor
+from modi.module.output_module.speaker import Speaker
+
 
 class TestModi(unittest.TestCase):
     """Tests for 'modi' class."""
@@ -21,15 +36,28 @@ class TestModi(unittest.TestCase):
         del self.modi
 
     def test_init(self):
+        """Test correct initialization of modi instance."""
         self.assertListEqual(self.modi._modules, list())
         self.assertDictEqual(self.modi._module_ids, dict())
 
     def test_exit(self):
+        """Test exit method."""
         self.modi.exit()
 
         self.modi._ser_proc.stop.assert_called_once_with()
         self.modi._par_proc.stop.assert_called_once_with()
         self.modi._exe_thrd.stop.assert_called_once_with()
+
+    def test_get_modules(self):
+        mock_input_values = (-1, -1, None)
+        self.modi._modules = [
+            SetupModule(*mock_input_values),
+            InputModule(*mock_input_values),
+            OutputModule(*mock_input_values),
+        ]
+
+        self.assertIsInstance(self.modi.modules, tuple)
+        self.assertTupleEqual(self.modi.modules, tuple(self.modi._modules))
 
 
 if __name__ == "__main__":
