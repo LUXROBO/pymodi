dd7de8601a2ab6802b79fef1e8693fc5aa03a23e
Jinsung Ha <jinsung.ha929@gmail.com>
2020-02-06 14:57:48 +0900

    Fix keywords' type in setup.py to list instead of tuple

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     include_package_data=True,
-    keywords=("pymodi", "modi", "luxrobo"),
+    keywords=["pymodi", "modi", "luxrobo"],
     name="pymodi",
     packages=find_namespace_packages(),
     setup_requires=setup_requirements,
