428063a92268d230a68d806d6ba154a8b9e82c15
Jinsung Ha <jinsung.ha929@gmail.com>
2020-02-06 19:36:37 +0900

    Refactor init file according to flake8 convention rule

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
-""" 
+"""
 DO NOT DELETE THIS __init__.py FILE.
 
 The file is required in order to correctly run the command below.
->>> python setup.py test 
+>>> python setup.py test
 
 Setup detects test_suite="tests", as a package.
 For tests/ to be recognized as a package, __init__.py is required.
