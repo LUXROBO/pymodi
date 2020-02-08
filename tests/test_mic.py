70aaddec83996dc1ab9c98a6a0a8febec455836f
Jinsung <jinsung.ha929@gmail.com>
2020-02-08 23:49:52 +0900

    Add template unittesting classes for mic and ultrasonic input modules

---
+++ tests/test_mic.py
@@ -0,0 +1,18 @@
+
+import unittest
+
+from unittest import mock
+
+from modi.module.input_module.mic import Mic
+
+
+class TestMic(unittest.TestCase):
+    """Tests for 'Mic' class."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
---
+++ tests/test_ultrasonic.py
@@ -0,0 +1,17 @@
+import unittest
+
+from unittest import mock
+
+from modi.module.input_module.ultrasonic import Ultrasonic
+
+
+class TestUltrasonic(unittest.TestCase):
+    """Tests for 'Ultrasonic' class."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
