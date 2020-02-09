f2d8373176349823f2c5c4fdc0dd8db29e9a6fd7
Jinsung Ha <jsung5381@naver.com>
2020-02-09 19:20:26 +0900

    Implement testcases for mic class

--- tests/test_mic.py
+++ tests/test_mic.py
@@ -11,8 +11,26 @@ class TestMic(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-        pass
+        mock_args = (-1, -1, None)
+        self.mic = Mic(*mock_args)
+        self.mic._get_property = mock.Mock()
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
-        pass
+        del self.mic
+
+    def test_get_volume(self):
+        """Test get_volume method."""
+        self.mic.get_volume()
+
+        self.mic._get_property.assert_called_once_with(
+            self.mic.PropertyType.VOLUME
+        )
+
+    def test_get_frequency(self):
+        """Test get_frequency method."""
+        self.mic.get_frequency()
+
+        self.mic._get_property.assert_called_once_with(
+            self.mic.PropertyType.FREQUENCY
+        )
