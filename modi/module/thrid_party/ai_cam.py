20cb21746b029799d76c21a9577934f0103516c4
Doyeon Kim <archdo0909@gmail.com>
2020-06-11 15:39:57 +0900

    Import opencv

---
+++ modi/module/thrid_party/ai_cam.py
@@ -0,0 +1,17 @@
+"""AI camera module."""
+
+import cv2
+
+
+class ai_cam():
+
+    def __init__(self):
+        pass
+    def isOpened(self):
+        pass
+    def read(self):
+        pass
+    def show(self):
+        pass
+    def write(self, frame):
+        pass
