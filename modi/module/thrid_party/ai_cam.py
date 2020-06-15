4cf38b9f1c9a9fa60af3692c18cafac29045d371
Doyeon Kim <archdo0909@gmail.com>
2020-06-15 18:51:13 +0900

    Add frame setter size of frame

--- modi/module/thrid_party/ai_cam.py
+++ modi/module/thrid_party/ai_cam.py
@@ -7,10 +7,18 @@ class AIcamera():
 
     def __init__(self, source: Union[int, str]):
         self.cap = cv2.VideoCapture(source)
+        # init video codec for raspberry pi 
+        self.fourcc = cv2.VideoWriter_fourcc(*"MPV4")
 
     def isOpened(self):
         return self.cap.isOpened()
-        
+
+    def set_frame_height(self, height):
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    def set_frame_weight(self, width):
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+
     def read(self):
         _, _frame = self.cap.read()
 
@@ -22,5 +30,6 @@ class AIcamera():
 
     def show(self):
         pass
+
     def write(self, frame):
         pass
