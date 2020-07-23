925426f61eeb9229a758334d81199d9d5c8fae90
Sebeom Lee <moses97@gmail.com>
2020-07-23 15:48:49 +0900

    Fix gyro example

--- examples/usage_examples/gyro_example.py
+++ examples/usage_examples/gyro_example.py
@@ -10,7 +10,12 @@ if __name__ == "__main__":
     gyro = bundle.gyros[0]
 
     while True:
-        print("Pitch: {0:<10}"
-              "Roll: {1:<10}"
-              "Yaw: {2:<10}".format(gyro.pitch, gyro.roll,
-                                    gyro.yaw), end='\r')
+        print(f"Pitch: {gyro.pitch:<10}"
+              f"Roll: {gyro.roll:<10}"
+              f"Yaw: {gyro.yaw:<10}"
+              f"Vel x: {gyro.angular_vel_x:<10}"
+              f"Vel y: {gyro.angular_vel_y:<10}"
+              f"Vel z: {gyro.angular_vel_z:<10}"
+              f"Acc x: {gyro.acceleration_x:<10}"
+              f"Acc y: {gyro.acceleration_y:<10}"
+              f"Acc z: {gyro.acceleration_z:<10}", end='\r')
