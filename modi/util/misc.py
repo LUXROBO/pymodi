0970d1bc2c33d9c32a1be8405afa901d3294a07a
Sebeom Lee <moses97@gmail.com>
2020-07-30 10:57:06 +0900

    Remove lazy getting feature

--- modi/util/misc.py
+++ modi/util/misc.py
@@ -1,5 +1,3 @@
-import time
-
 from modi.module.input_module.button import Button
 from modi.module.input_module.dial import Dial
 from modi.module.input_module.env import Env
@@ -68,26 +66,10 @@ def get_module_from_name(module_type: str):
 
 class module_list(list):
 
-    def __init__(self, src, module_type, lazy=True):
+    def __init__(self, src, module_type=None):
         self.__src = src
         self.__module_type = module_type
-        self.__lazy = lazy
         super().__init__(self.sublist())
-
-    def __getitem__(self, item):
-        """ When accessing the module, the modules are sorted in an
-        ascending order of
-        1. the distance from network module
-        2. left to right
-        3. up to down
-
-        :param item: Index of the module
-        :return: Module
-        """
-        if self.__lazy:
-            while not len(self.sublist()) > item:
-                time.sleep(0.1)
-        return self.sublist()[item]
 
     def get(self, module_id):
         for module in self.sublist():
@@ -96,8 +78,19 @@ class module_list(list):
         raise Exception("Module with given id does not exits!!")
 
     def sublist(self):
-        modules = [module for module in self.__src
-                   if module.module_type == self.__module_type]
+        """ When accessing the module, the modules are sorted in an
+        ascending order of
+        1. the distance from network module
+        2. left to right
+        3. up to down
+
+        :return: Module
+        """
+        if self.__module_type:
+            modules = [module for module in self.__src
+                       if module.module_type == self.__module_type]
+        else:
+            modules = self.__src
         modules.sort()
         return modules
 
