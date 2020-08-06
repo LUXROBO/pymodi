3c6060763290300fdc2e7b39dbb261f27bb8d89b
Sebeom Lee <moses97@gmail.com>
2020-08-06 18:47:57 +0900

    Fix battery module

--- modi/module/setup_module/battery.py
+++ modi/module/setup_module/battery.py
@@ -1,10 +1,7 @@
-"""Network module."""
-
-from enum import IntEnum
+"""Battery module."""
 
 from modi.module.setup_module.setup_module import SetupModule
 
 
 class Battery(SetupModule):
-    class PropertyType(IntEnum):
-        RESERVED = 0
+    pass
