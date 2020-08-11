d11388a159aebf8f63e0cb52837860f38ffe917d
k2sebeom <moses97@gmail.com>
2020-08-11 16:02:39 +0900

    Add getter for network module

--- modi/modi.py
+++ modi/modi.py
@@ -81,10 +81,13 @@ class MODI:
     def modules(self) -> module_list:
         """Module List of connected modules except network module.
         """
-        return module_list(
-            list(filter(lambda module: module.module_type != 'Network',
-                        self._modules))
-        )
+        return module_list(self._modules)
+
+    @property
+    def network(self):
+        for module in self._modules:
+            if module.module_type == 'Network':
+                return module
 
     @property
     def buttons(self) -> module_list:
