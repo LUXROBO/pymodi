ee4719c9a794572623ca3d9ae9ccef7d5529d290
k2sebeom <moses97@gmail.com>
2020-08-13 10:29:31 +0900

    Remove enum dependency from input modules

--- modi/module/input_module/ultrasonic.py
+++ modi/module/input_module/ultrasonic.py
@@ -1,14 +1,11 @@
 """Ultrasonic module."""
 
-from enum import IntEnum
-
 from modi.module.input_module.input_module import InputModule
 
 
 class Ultrasonic(InputModule):
 
-    class PropertyType(IntEnum):
-        DISTANCE = 2
+    DISTANCE = 2
 
     @property
     def distance(self) -> float:
@@ -17,4 +14,4 @@ class Ultrasonic(InputModule):
         :return: Distance to object.
         :rtype: float
         """
-        return self._get_property(self.PropertyType.DISTANCE)
+        return self._get_property(Ultrasonic.DISTANCE)
