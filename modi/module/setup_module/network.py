5b999d82e35b0a6cae18d461128a75519a1d8d41
k2sebeom <moses97@gmail.com>
2020-08-13 10:41:18 +0900

    Remove enum dependency from tasks

--- modi/module/setup_module/network.py
+++ modi/module/setup_module/network.py
@@ -1,31 +1,28 @@
 """Network module."""
 import time
 
-from enum import IntEnum
-
 from modi.module.setup_module.setup_module import SetupModule
 from modi.util.msgutil import parse_message
 
 
 class Network(SetupModule):
-    class PropertyType(IntEnum):
-        BUTTON = 3
-        JOYSTICK = 3
-        DIAL = 4
-        LEFT_SLIDER = 5
-        RIGHT_SLIDER = 6
-        TIMER = 7
-        BUTTON_CLICK = 0x0102
-        BUTTON_DOUBLE_CLICK = 0x0103
-        TOGGLE = 0x00104
 
-    class CommandType(IntEnum):
-        BUZZER = 0x0003
-        CAMERA = 0x0101
+    BUTTON = 3
+    JOYSTICK = 3
+    DIAL = 4
+    LEFT_SLIDER = 5
+    RIGHT_SLIDER = 6
+    TIMER = 7
+    BUTTON_CLICK = 0x0102
+    BUTTON_DOUBLE_CLICK = 0x0103
+    TOGGLE = 0x00104
+
+    BUZZER = 0x0003
+    CAMERA = 0x0101
 
     @property
     def button_pressed(self):
-        return self._get_property(self.PropertyType.BUTTON) == 100
+        return self._get_property(Network.BUTTON) == 100
 
     @property
     def joystick(self):
@@ -34,35 +31,35 @@ class Network(SetupModule):
             30.0: 'down',
             40.0: 'left',
             50.0: 'right',
-        }.get(self._get_property(self.PropertyType.JOYSTICK))
+        }.get(self._get_property(Network.JOYSTICK))
 
     @property
     def dial(self):
-        return self._get_property(self.PropertyType.DIAL)
+        return self._get_property(Network.DIAL)
 
     @property
     def left_slider(self):
-        return self._get_property(self.PropertyType.LEFT_SLIDER)
+        return self._get_property(Network.LEFT_SLIDER)
 
     @property
     def right_slider(self):
-        return self._get_property(self.PropertyType.RIGHT_SLIDER)
+        return self._get_property(Network.RIGHT_SLIDER)
 
     @property
     def timer(self):
-        return self._get_property(self.PropertyType.TIMER) == 100
+        return self._get_property(Network.TIMER) == 100
 
     @property
     def button_clicked(self):
-        return self._get_property(self.PropertyType.BUTTON_CLICK) == 100
+        return self._get_property(Network.BUTTON_CLICK) == 100
 
     @property
     def button_double_clicked(self):
-        return self._get_property(self.PropertyType.BUTTON_DOUBLE_CLICK) == 100
+        return self._get_property(Network.BUTTON_DOUBLE_CLICK) == 100
 
     @property
     def button_toggled(self):
-        return self._get_property(self.PropertyType.BUTTON.TOGGLE) == 100
+        return self._get_property(Network.TOGGLE) == 100
 
     def _set_property(self, command_type, value):
         self._conn.send(parse_message(
@@ -71,10 +68,10 @@ class Network(SetupModule):
         time.sleep(0.05)
 
     def take_picture(self):
-        self._set_property(self.CommandType.CAMERA, 100)
+        self._set_property(Network.CAMERA, 100)
 
     def buzzer_on(self):
-        self._set_property(self.CommandType.BUZZER, 100)
+        self._set_property(Network.BUZZER, 100)
 
     def buzzer_off(self):
-        self._set_property(self.CommandType.BUZZER, 0)
+        self._set_property(Network.BUZZER, 0)
