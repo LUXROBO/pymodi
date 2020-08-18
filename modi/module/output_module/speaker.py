ac3d95ce4cf5ef42b73be262e4d12c874559b7e3
k2sebeom <moses97@gmail.com>
2020-08-18 10:34:11 +0900

    Enable playing a tune by string name

--- modi/module/output_module/speaker.py
+++ modi/module/output_module/speaker.py
@@ -11,6 +11,33 @@ class Speaker(OutputModule):
 
     SET_TUNE = 16
 
+    SCALE_TABLE = {
+        'FA5': 698,
+        'SOL5': 783,
+        'LA5': 880,
+        'TI5': 988,
+        'DO#5': 554,
+        'RE#5': 622,
+        'FA#5': 739,
+        'SOL#5': 830,
+        'LA#5': 932,
+        'DO6': 1046,
+        'RE6': 1174,
+        'MI6': 1318,
+        'FA6': 1397,
+        'SOL6': 1567,
+        'LA6': 1760,
+        'TI6': 1975,
+        'DO#6': 1108,
+        'RE#6': 1244,
+        'FA#6': 1479,
+        'SOL#6': 1661,
+        'LA#6': 1864,
+        'DO7': 2093,
+        'RE7': 2349,
+        'MI7': 2637
+    }
+
     def __init__(self, id_, uuid, conn_task):
         super().__init__(id_, uuid, conn_task)
         self.frequency = 880  # Default frequency of A6
@@ -33,6 +60,16 @@ class Speaker(OutputModule):
         """
         if tune_value == self.tune:
             return
+
+        if isinstance(tune_value[0], str):
+            tune_value = (
+                Speaker.SCALE_TABLE.get(tune_value[0], -1),
+                tune_value[1]
+            )
+
+        if tune_value[0] < 0:
+            raise ValueError("Not a supported frequency value")
+
         self._set_property(
             self._id,
             Speaker.SET_TUNE,
