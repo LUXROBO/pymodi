f6d8f0ff6641e3a50e8ed7f4aad1d06fe6806189
Jinsung Ha <jsung5381@naver.com>
2020-09-23 17:15:59 +0900

    Comment ampy-related codes out

--- modi/util/upython.py
+++ modi/util/upython.py
@@ -1,20 +1,20 @@
 import serial.tools.list_ports as stl
-from ampy.pyboard import Pyboard
-from ampy.files import Files
-
-
-def __get_upload_port():
-    com_ports = stl.comports()
-    devices = [port.device for port in com_ports]
-    devices.sort()
-    return devices[-1]
-
-
-def upload_file(local_path: str, remote_path: str) -> None:
-    esp_port = __get_upload_port()
-    print(f"Found esp device at port {esp_port}")
-    pyb = Pyboard(esp_port, rawdelay=1)
-    f = Files(pyb)
-    print(f.ls())
-    with open(local_path, 'rb') as local_file:
-        f.put(remote_path, local_file.read())
+#from ampy.pyboard import Pyboard
+#from ampy.files import Files
+#
+#
+#def __get_upload_port():
+#    com_ports = stl.comports()
+#    devices = [port.device for port in com_ports]
+#    devices.sort()
+#    return devices[-1]
+#
+#
+#def upload_file(local_path: str, remote_path: str) -> None:
+#    esp_port = __get_upload_port()
+#    print(f"Found esp device at port {esp_port}")
+#    pyb = Pyboard(esp_port, rawdelay=1)
+#    f = Files(pyb)
+#    print(f.ls())
+#    with open(local_path, 'rb') as local_file:
+#        f.put(remote_path, local_file.read())
