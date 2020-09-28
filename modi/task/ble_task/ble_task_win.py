58557c4c106bb2bfd77f1f6799892383a2248c08
Jinsung Ha <jsung5381@naver.com>
2020-09-28 12:35:27 +0900

    Update bleak 0.8.0 compatible for Windows BLE

--- modi/task/ble_task/ble_task_win.py
+++ modi/task/ble_task/ble_task_win.py
@@ -39,7 +39,7 @@ class BleTask(ConnTask):
             return None
 
     async def __connect(self, address):
-        client = BleakClient(address, self._loop)
+        client = BleakClient(address, timeout=3)
         await client.connect(timeout=1)
         return client
 
