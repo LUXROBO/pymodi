d775db00c2218ef70a37c2f1344d0550951b8cdd
Jinsung Ha <jsung5381@naver.com>
2020-10-08 16:10:29 +0900

    Refactor parentheses

--- modi/util/conn_util.py
+++ modi/util/conn_util.py
@@ -16,7 +16,8 @@ def list_modi_ports() -> List[ListPortInfo]:
             port.manufacturer == "LUXROBO"
             or port.product == "MODI Network Module"
             or port.product == "MODI Network Module(BootLoader)"
-            or (port.vid == 0x2fdc and port.pid == 0x2))
+            or (port.vid == 0x2fdc and port.pid == 0x2)
+        )
 
     return [port for port in stl.comports() if __is_modi_port(port)]
 
