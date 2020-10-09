ca4587758b323c9adee9b1730c635a899d7fa042
Jinsung Ha <jsung5381@naver.com>
2020-10-09 19:25:28 +0900

    Remove abstract python 3 support statement

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         "Intended Audience :: Education",
         'Intended Audience :: Information Technology',
         'Intended Audience :: Science/Research',
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
