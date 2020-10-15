a44d6a7885f4d177b8a39259a45ffb8ede5f476b
Jinsung Ha <jsung5381@naver.com>
2020-10-15 20:02:06 +0900

    Fix issue with module_image_path

--- modi/util/gui_firmware_updater.py
+++ modi/util/gui_firmware_updater.py
@@ -67,16 +67,15 @@ class Form(QtWidgets.QDialog):
         self.ui.push_button.setFocus(True)
 
         # Init module image
-        module_pixmap = QPixmap(
-            os.path.join(
-                os.path.dirname(__file__),
-                '..', 'assets', 'image', 'network.png'
-            )
+        module_image_path = os.path.join(
+            os.path.dirname(__file__),
+            '..', 'assets', 'image', 'network.png'
         )
         if installer:
-            module_pixmap = os.path.dirname(__file__).replace(
+            module_image_path = os.path.dirname(__file__).replace(
                 'util', 'network.png'
             )
+        module_pixmap = QPixmap(module_image_path)
         self.ui.curr_module_img.setPixmap(module_pixmap)
 
         # Init password field for enabling developer mode
