7156c4dabdba2e16f1d308c7c6d5aea8b6df3e1a
Jinsung Ha <jsung5381@naver.com>
2020-10-20 11:00:38 +0900

    Evince that PyMODI is an OS independent software

--- setup.py
+++ setup.py
@@ -53,5 +53,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
