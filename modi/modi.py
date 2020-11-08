dd6ec86e0cc374a51bd06725e69156043f483235
Jinsung Ha <jsung5381@naver.com>
2020-11-08 23:16:13 +0900

    Fix convention issue with logger

--- modi/modi.py
+++ modi/modi.py
@@ -27,6 +27,8 @@ class MODI:
     ):
         # Init logger
         __logger = self.__init_logger()
+        __logger.info('modi version:', modi_version)
+        __logger.info('connection type:', conn_type)
 
         if virtual_modules and conn_type != "vir":
             raise ValueError(
@@ -90,11 +92,12 @@ class MODI:
         logger.setLevel(logging.DEBUG)
 
         formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s'
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
         file_handler = logging.FileHandler('pymodi.log')
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
+
         logger.addHandler(file_handler)
         return logger
 
