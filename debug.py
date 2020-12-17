523fe1121a5382c6bbc8f6619596ca37c7660f88
Jinsung Ha <jsung5381@naver.com>
2020-12-17 16:58:35 +0900

    Add debug code with a simple guide line

--- debug.py
+++ debug.py
@@ -1,8 +1,11 @@
 import modi
 import time
 
+"""
+Write pymodi code to debug, set an appropriate break point, start debugging!
+"""
 if __name__ == '__main__':
-    bundle = modi.MODI(conn_type='ser')
+    bundle = modi.MODI(verbose=True)
 
     while True:
         time.sleep(0.01)
