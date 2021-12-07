f20f46c3b1682bb4c12d942d25a3c046630ea473
Jinsung Ha <jsung5381@naver.com>
2021-12-07 16:15:13 +0900

    Implement modi ws client, instead of ws server

--- modi/task/soc_task.py
+++ modi/task/soc_task.py
@@ -1,18 +1,15 @@
 
-import time
 
-from queue import Queue
-from threading import Thread
-
-from websocket_server import WebsocketServer
+from websocket import create_connection
 
 from modi.task.conn_task import ConnTask
 
 
 class SocTask(ConnTask):
     """
-    SocTask implements a websocket server.
-    Thus, PyMODI works as a websocket server, listening to a MODI Client.
+    SocTask implements a websocket client.
+    Thus, PyMODI works as a websocket client, listening to a MODI Client.
+    However, it's possible for PyMODI client to connect to a MODI relay server.
     A connection between a server and a client must be one-to-one.
     """
 
@@ -20,73 +17,27 @@ class SocTask(ConnTask):
         super().__init__(verbose=verbose)
         print('Initiating soc_task connection...')
         DEFAULT_SOC_PORT = 8765
-        port = port if port else DEFAULT_SOC_PORT
-        self._bus = WebsocketServer(host='', port=port)
-        self._recv_q = Queue()
-        self._send_q = Queue()
-        self.__close_event = False
+        self.port = port if port else DEFAULT_SOC_PORT
 
     def open_conn(self):
-        Thread(target=self.__open, daemon=True).start()
-        Thread(target=self.__send_handler, daemon=True).start()
+        self._bus = create_connection(f'ws://localhost:{self.port}')
 
     def close_conn(self):
-        self.__close_event = True
-        self._bus.shutdown_gracefully()
+        self._bus.close()
 
     def recv(self):
-        if self._recv_q.empty():
-            return None
-        json_pkt = self._recv_q.get()
+        json_pkt = self._bus.recv()
         if self.verbose:
             print(f'recv: {json_pkt}')
         return json_pkt
 
     @ConnTask.wait
     def send(self, pkt):
-        self._send_q.put(pkt.encode())
-        while not self._send_q.empty():
-            time.sleep(0.01)
+        self._bus.send(pkt.encode())
         if self.verbose:
             print(f'send: {pkt}')
 
-    def send_nowait(self, pkt: str) -> None:
-        self._send_q.put(pkt.encode())
+    def send_nowait(self, pkt):
+        self._bus.send(pkt.encode())
         if self.verbose:
             print(f'send: {pkt}')
-
-    #
-    # Helper Methods
-    #
-    def __open(self):
-        def new_client(client, server):
-            server.send_message_to_all(
-                f'Hey all, a new client:{client} has joined us'
-            )
-
-        def client_left(client, server):
-            server.send_message_to_all(
-                f'Hey all, a client:{client} has left us'
-            )
-
-        # Set callback functions
-        self._bus.set_fn_new_client(new_client)
-        self._bus.set_fn_message_received(self.__recv_handler)
-        self._bus.set_fn_client_left(client_left)
-
-        # Run the server forever
-        self._bus.run_forever()
-
-    def __recv_handler(self, client, server, message):
-        self._recv_q.put(message)
-
-    def __send_handler(self):
-        while not self.__close_event:
-            if self._send_q.empty():
-                time.sleep(0.001)
-                continue
-            try:
-                message = self._send_q.get()
-                self._bus.send_message_to_all(message)
-            except Exception:
-                self.__close_event = True
